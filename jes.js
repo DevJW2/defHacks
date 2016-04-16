dcb7c03e5a507b33f52fcb9d4b1f66d1bfa55108
Jeffrey Weng <mcjeffrey168@gmail.com>
2016-04-16 14:22:31 -0400

    new changes

--- jes.js
+++ jes.js
@@ -1,3 +1,10 @@
 var ECanvas = document.getElementById("templateCanvas");
 var CTX = ECanvas.getContext("2d");
 
+
+function myFunction(event) {
+    var x = event.which || event.keyCode; // event.keyCode is used for IE8 and earlier
+    if (x == 27) {  // 27 is the ESC key
+        alert ("You pressed the Escape key!");
+    }
+}
